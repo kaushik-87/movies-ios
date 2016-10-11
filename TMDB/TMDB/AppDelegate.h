b9c041dfb07b78084d030ef09896e5130317f01b
Kaushik87 <urskaush@gmail.com>
2016-10-10 22:11:02 -0700

    Added comments about the application.

--- TMDB/TMDB/AppDelegate.h
+++ TMDB/TMDB/AppDelegate.h
@@ -6,6 +6,24 @@
 //  Copyright © 2016 Home. All rights reserved.
 //
 
+/*
+ 
+ This application is designed and developed to list the upcoming movies using TMDB.
+ 
+ Application has two main screens:
+ 
+ 1. List of Movies
+ 2. Details of the selcted Movie. 
+ 
+ For this application we are restricting the movie list count to 50. All the movies are sorted by the release date in ascending order.
+ In the list view each movie is displayed in a row with title, poster image, genres and release date. 
+ 
+ Selecting on each row navigates to detailed screen. Detailed screen displays Movie title, bigger poster image, genres, release date and oveview about the movie.
+ 
+ The application is desgined in such a way that it can extensible for more number of movies and also can be sorted using other parameters like name, popularity etc.
+ 
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
